e8eb26a2f7fbac12a84914572c84d8ff86499f37
Will Fritz <Will Fritz>
2016-12-23 13:17:44 -0500

    school stats

--- 09-10 school stats.py
+++ 09-10 school stats.py
@@ -7,7 +7,6 @@ url = "http://www.sports-reference.com/cbb/seasons/2010-school-stats.html"
 # query website and return the html
 page = urllib2.urlopen(url)
 
-#parse html and put it in Beautiful Soup format
 soup = BeautifulSoup(page, "html.parser")
 
 # get data from datatable
